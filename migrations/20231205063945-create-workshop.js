0fa9e6eda4617ab867bb279335b45ae8255f2c9f
michael mbugua <mikembugua960@gmail.com>
2023-12-05 09:46:39 +0300

    generate workshop migrations

---
+++ migrations/20231205063945-create-workshop.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Workshops', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      posterUrl: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.TIME
+      },
+      capacity: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.DOUBLE
+      },
+      capacityStatus: {
+        type: Sequelize.BOOLEAN
+      },
+      ageGroup: {
+        type: Sequelize.STRING
+      },
+      ageMin: {
+        type: Sequelize.INTEGER
+      },
+      ageMax: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Workshops');
+  }
+};
