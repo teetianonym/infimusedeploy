e381a535cf1002e0418dcfcf264beadc49441d78
michael mbugua <mikembugua960@gmail.com>
2023-12-05 19:28:18 +0300

    associating the models

--- models/classticket.js
+++ models/classticket.js
@@ -1,24 +1,22 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class classTicket extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  classTicket.init({
+"use strict";
+const { DataTypes } = require("sequelize");
+const db = require("./../config/db");
+
+const ClassTicket = db.define(
+  "ClassTicket",
+  {
     ticketId: DataTypes.STRING,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'classTicket',
-  });
-  return classTicket;
+    completed: DataTypes.BOOLEAN,
+    customerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+ClassTicket.associate = (models) => {
+  ClassTicket.belongsTo(models.Customer, { foreignKey: "customerId" });
 };
+
+module.exports = ClassTicket;
