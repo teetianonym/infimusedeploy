0ead60f9f9145c600eec96c47eba0b7e9ab4f57f
michael mbugua <mikembugua960@gmail.com>
2023-12-06 19:58:13 +0300

    import custom routes

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const guestRoutes = require("./routes/guestRoutes");
 const customerRoutes = require("./routes/customerRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
 const reviewCommentRoutes = require("./routes/reviewCommentRoutes");
+const classTicketRoutes = require("./routes/classTicketRoutes");
+const locationRoutes = require("./routes/locationRoutes");
+const packageTicketRoutes = require("./routes/packageTicketRoutes");
+const paymentTransactionRoute = require("./routes/paymentTransactionRoute");
+const payoutRoutes = require("./routes/payoutRoutes");
+const waitlistRoutes = require("./routes/waitlistRoutes");
+const workshopTicketRoutes = require("./routes/workshopTicketRoute");
 
 const app = express();
 dotenv.config();
@@ -33,16 +40,34 @@ db.authenticate()
 app.use(express.json());
 app.use(cors(corsOption));
 
-app.use("/api/v1/hosts", hostRoutes);
+// classess sessions and workshops
 app.use("/api/v1/class-sessions", classSessionRoutes);
 app.use("/api/v1/package-sessions", packageSessionRoutes);
+app.use("/api/v1/workshop-classes", workshopClassRoutes);
 app.use("/api/v1/package-classes", packageClassRoutes);
 app.use("/api/v1/workshops", workshopRoutes);
-app.use("/api/v1/workshop-classes", workshopClassRoutes);
-app.use("/api/v1/guests", guestRoutes);
+
+// users
+app.use("/api/v1/hosts", hostRoutes);
 app.use("/api/v1/customers", customerRoutes);
+app.use("/api/v1/guests", guestRoutes);
+
+// tickets
+app.use("/api/v1/class-tickets", classTicketRoutes);
+app.use("/api/v1/package-tickets", packageTicketRoutes);
+app.use("/api/v1/workshop-tickets", workshopTicketRoutes);
+
+// payments
+app.use("/api/v1/payouts", payoutRoutes);
+app.use("/api/v1/transactions", paymentTransactionRoute);
+
+// notifications and comments
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/review-comments", reviewCommentRoutes);
+
+// others
+app.use("/api/v1/locations", locationRoutes);
+app.use("/api/v1/waitlists", waitlistRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
