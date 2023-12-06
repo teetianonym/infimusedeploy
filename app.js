d49529086b3e922e440cc50cef7d83155ef26be1
michael mbugua <mikembugua960@gmail.com>
2023-12-06 17:00:07 +0300

    import routes and apply the middleware

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const db = require("./config/db");
 const hostRoutes = require("./routes/hostRoutes");
 const classSessionRoutes = require("./routes/classSessionRoutes");
 const packageSessionRoutes = require("./routes/packageSessionRoute");
+const workshopClassRoutes = require("./routes/workshopClassRoutes");
+const workshopRoutes = require("./routes/workshopRoutes");
+const packageClassRoutes = require("./routes/packageClassRoutes");
+const guestRoutes = require("./routes/guestRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const reviewCommentRoutes = require("./routes/reviewCommentRoutes");
 
 const app = express();
 dotenv.config();
@@ -29,6 +36,13 @@ app.use(cors(corsOption));
 app.use("/api/v1/hosts", hostRoutes);
 app.use("/api/v1/class-sessions", classSessionRoutes);
 app.use("/api/v1/package-sessions", packageSessionRoutes);
+app.use("/api/v1/package-classes", packageClassRoutes);
+app.use("/api/v1/workshops", workshopRoutes);
+app.use("/api/v1/workshop-classes", workshopClassRoutes);
+app.use("/api/v1/guests", guestRoutes);
+app.use("/api/v1/customers", customerRoutes);
+app.use("/api/v1/notifications", notificationRoutes);
+app.use("/api/v1/review-comments", reviewCommentRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
