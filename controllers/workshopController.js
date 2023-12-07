311cfb1f37340f0988a1187e3a347598cf4a610b
michael mbugua <mikembugua960@gmail.com>
2023-12-08 00:17:23 +0300

    create  find method with other models associated

--- controllers/workshopController.js
+++ controllers/workshopController.js
@@ -2,9 +2,55 @@ const factory = require("./factory");
 const db = require("./../models");
 
 const Workshop = db.workshops;
+const workshopTicket = db.workshopTickets;
+const Customer = db.customers;
+const Guest = db.guests;
 
 exports.createWorkshop = factory.createDoc(Workshop);
 exports.getAllWorkshop = factory.getAllDocs(Workshop);
 exports.updateWorkshop = factory.updateDoc(Workshop);
-exports.getWorkshop = factory.getOneDoc(Workshop);
 exports.deleteWorkshop = factory.deleteDoc(Workshop);
+
+exports.getWorkshop = async (req, res) => {
+  try {
+    const doc = await Workshop.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: workshopTicket,
+
+          as: "workshopTicket",
+          attributes: [
+            "ticketId",
+            "GuestId",
+            "paymentTransactionId",
+            "customerId",
+          ],
+          include: [
+            {
+              model: Customer,
+              as: "customer",
+              attributes: ["firstName", "lastName", "email"],
+            },
+          ],
+          include: [
+            {
+              model: Guest,
+              as: "guest",
+              attributes: ["firstName", "lastName", "email"],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (doc) {
+      const ticketCount = doc.workshopTicket.length;
+      res
+        .status(200)
+        .json({ result: "Success", workshopTickets: ticketCount, Data: doc });
+    }
+  } catch (error) {
+    res.status(500).json({ Error: error });
+  }
+};
