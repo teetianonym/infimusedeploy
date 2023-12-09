e2e83c7b5a59ffe36d67782ff3df4f4ea3531979
michael mbugua <mikembugua960@gmail.com>
2023-12-09 23:20:40 +0300

    add filter methods

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,34 @@
 const factory = require("./factory");
 const db = require("./../models");
 const Customer = db.customers;
+const workshopTicket = db.workshopTickets;
+const ClassTicket = db.classTickets;
+const PackageSession = db.packageSessions;
 
 exports.createCustomer = factory.createDoc(Customer);
-exports.getCustomer = factory.getOneDoc(Customer);
+// exports.getCustomer = factory.getOneDoc(Customer);
 exports.getAllCustomers = factory.getAllDocs(Customer);
 exports.updateCustomer = factory.updateDoc(Customer);
 exports.deleteCustomer = factory.deleteDoc(Customer);
+
+exports.getCustomer = async (req, res) => {
+  try {
+    const doc = await Customer.findByPk(req.params.id, {
+      include: [
+        {
+          model: PackageSession,
+
+          as: "packageSession",
+          attributes: ["startTime"],
+        },
+      ],
+    });
+
+    if (doc) {
+      res.status(200).json({ result: "Success", Data: doc });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error });
+  }
+};
