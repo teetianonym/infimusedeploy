70fa79c325a4a1cfa65aa553bee0bff53c0de081
michael mbugua <mikembugua960@gmail.com>
2023-12-11 22:49:27 +0300

    get one classSession with all the tickets

--- controllers/classSessionController.js
+++ controllers/classSessionController.js
@@ -2,9 +2,31 @@ const db = require("./../models");
 const factory = require("./factory");
 
 const ClassSession = db.classSessions;
+const ClassTicket = db.classTickets;
 
 exports.createClassSession = factory.createDoc(ClassSession);
 exports.getAllClassSession = factory.getAllDocs(ClassSession);
-exports.getOneClassSession = factory.getOneDoc(ClassSession);
 exports.updateClassSession = factory.updateDoc(ClassSession);
 exports.deleteClassSession = factory.deleteDoc(ClassSession);
+
+exports.getOneClassSession = async (req, res) => {
+  const classSessionId = req.params.id;
+  try {
+    const doc = await ClassSession.findByPk(classSessionId, {
+      include: [
+        // {
+        //   model: ClassTicket,
+        //   as: "ClassTicket",
+        //   //   attributes: ["ticketId"],
+        // },
+      ],
+    });
+
+    if (doc) {
+      res.status(200).json({ result: "Success", Data: doc });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error });
+  }
+};
