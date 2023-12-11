1632d74ea04d7270540e862e4ac3616827583536
michael mbugua <mikembugua960@gmail.com>
2023-12-11 22:06:27 +0300

    add host controller actions

--- controllers/hostController.js
+++ controllers/hostController.js
@@ -1,9 +1,31 @@
 const factory = require("./factory");
 const db = require("./../models");
+const hostReview = db.hostReviews;
 const Host = db.hosts;
 
 exports.createHost = factory.createDoc(Host);
 exports.getAllHost = factory.getAllDocs(Host);
 exports.updateHost = factory.updateDoc(Host);
-exports.getHost = factory.getOneDoc(Host);
 exports.deleteHost = factory.deleteDoc(Host);
+
+exports.getHost = async (req, res) => {
+  const hostId = req.params.id;
+  try {
+    const doc = await Host.findByPk(hostId, {
+      include: [
+        {
+          model: hostReview,
+          attributes: ["content", "rating"],
+          as: "hostReview",
+        },
+      ],
+    });
+
+    if (doc) {
+      res.status(200).json({ result: "Success", Data: doc });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error });
+  }
+};
