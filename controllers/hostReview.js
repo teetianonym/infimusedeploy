d90a8c75f75b6bdde1df228cd7686d922ce13b35
michael mbugua <mikembugua960@gmail.com>
2023-12-13 22:29:22 +0300

    minor changes

--- controllers/hostReview.js
+++ controllers/hostReview.js
@@ -2,8 +2,23 @@ const factory = require("./factory");
 const db = require("./../models");
 
 const hostReview = db.hostReviews;
-exports.createhostReview = factory.createDoc(hostReview);
+// exports.createhostReview = factory.createDoc(hostReview);
 exports.gethostReview = factory.getOneDoc(hostReview);
 exports.getAllhostReviews = factory.getAllDocs(hostReview);
 exports.updatehostReview = factory.updateDoc(hostReview);
 exports.deletehostReview = factory.deleteDoc(hostReview);
+
+exports.createhostReview = async (req, res) => {
+  try {
+    const doc = await hostReview.create(req.body);
+    console.log("req.body :" + req.body);
+    res.status(200).json({ status: "Document created successful", doc });
+  } catch (error) {
+    console.log(error);
+    if (error.name === "SequelizeValidationError") {
+      res
+        .status(400)
+        .json({ status: "Validation error", errors: error.errors });
+    }
+  }
+};
