9166e60faefc3f504e7645c220a5e2a80320e60e
michael mbugua <mikembugua960@gmail.com>
2023-12-13 23:12:46 +0300

    filtering locations

--- controllers/packageClassController.js
+++ controllers/packageClassController.js
@@ -4,6 +4,7 @@ const db = require("./../models");
 const PackageClass = db.packageClasses;
 const PackageTicket = db.packageTickets;
 const Comment = db.comments;
+const Location = db.locations;
 
 exports.createPackageClass = factory.createDoc(PackageClass);
 exports.getAllPackageClass = factory.getAllDocs(PackageClass);
@@ -34,6 +35,44 @@ exports.getPackageClass = async (req, res) => {
     if (doc) {
       res.status(200).json({ result: "Success", Data: doc });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ Error: error });
+  }
+};
+
+exports.getAllPackageClass = async (req, res) => {
+  try {
+    // paginate
+    const page = parseInt(req.query.page, 15) || 1;
+    const limit = parseInt(req.query.limit, 15) || 15;
+    const offset = (page - 1) * limit;
+    // sorting
+    const sortFields = ["location", "price"];
+    const sortBy = req.query.sort;
+    const order = sortFields.includes(sortBy) ? sortBy : "createdAt";
+
+    // location Filter
+    const { location } = req.query;
+    let whereClause = {};
+    if (location) {
+      const locations = await Location.findAll({
+        where: { location },
+      });
+      const packageIds = locations.map((location) => location.packageClassId);
+
+      whereClause = { id: packageIds };
+    }
+
+    const docs = await PackageClass.findAll({
+      limit: limit,
+      offset: offset,
+      order: [[order, "DESC"]],
+      where: whereClause,
+    });
+    res
+      .status(200)
+      .json({ result: "Success", TotalDocs: docs.length, Document: docs });
   } catch (error) {
     console.log(error);
     res.status(500).json({ Error: error });
