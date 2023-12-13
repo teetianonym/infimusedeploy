a59b2c2dc0264c7fa63a62a821f5a406912e0996
michael mbugua <mikembugua960@gmail.com>
2023-12-13 22:30:15 +0300

    remove redandunt code

--- controllers/workshopController.js
+++ controllers/workshopController.js
@@ -2,9 +2,8 @@ const factory = require("./factory");
 const db = require("./../models");
 
 const Workshop = db.workshops;
+const Comment = db.comments;
 const workshopTicket = db.workshopTickets;
-const Customer = db.customers;
-const Guest = db.guests;
 
 exports.createWorkshop = factory.createDoc(Workshop);
 exports.getAllWorkshop = factory.getAllDocs(Workshop);
@@ -13,44 +12,29 @@ exports.deleteWorkshop = factory.deleteDoc(Workshop);
 
 exports.getWorkshop = async (req, res) => {
   try {
-    const doc = await Workshop.findOne({
-      where: { id: req.params.id },
+    const doc = await Workshop.findByPk(req.params.id, {
       include: [
         {
           model: workshopTicket,
 
           as: "workshopTicket",
-          attributes: [
-            "ticketId",
-            "GuestId",
-            "paymentTransactionId",
-            "customerId",
-          ],
-          include: [
-            {
-              model: Customer,
-              as: "customer",
-              attributes: ["firstName", "lastName", "email"],
-            },
-          ],
-          include: [
-            {
-              model: Guest,
-              as: "guest",
-              attributes: ["firstName", "lastName", "email"],
-            },
-          ],
+          attributes: ["ticketId"],
+        },
+      ],
+      include: [
+        {
+          model: Comment,
+          as: "comment",
+          attributes: ["comment"],
         },
       ],
     });
 
     if (doc) {
-      const ticketCount = doc.workshopTicket.length;
-      res
-        .status(200)
-        .json({ result: "Success", workshopTickets: ticketCount, Data: doc });
+      res.status(200).json({ result: "Success", Data: doc });
     }
   } catch (error) {
+    console.log(error);
     res.status(500).json({ Error: error });
   }
 };
